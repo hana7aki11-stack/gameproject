fd44355e2fc368ae898353827647ec1ec8704766
manaha1024 <hana7aki11@gmail.com>
2026-05-22 11:47:06 +0900

    fix image and url

--- gameproject/gameproject/settings.py
+++ gameproject/gameproject/settings.py
@@ -125,6 +125,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
